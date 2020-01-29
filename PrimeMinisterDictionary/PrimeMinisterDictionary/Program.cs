51569b96ec365aec6867cb89e6c2edbb46245a4f
priyadarshanjena <59999152+priyadarshanjena@users.noreply.github.com>
2020-01-29 16:51:06 +0530

    Add files via upload

---
+++ PrimeMinisterDictionary/PrimeMinisterDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeMinisterDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var PrimeMinister = new Dictionary<int, String>()
+            {
+                 { 1998,"Atal Bihari Vajpayee" },
+                 { 2014,"Narendra Modi" },
+                 { 2004,"Manmohan Singh"},
+             };
+            Console.WriteLine("Prime Minister of 2004 is :" + PrimeMinister[2004]);
+            PrimeMinister.Add(2020,"Narendra Modi");
+            var list = PrimeMinister.Keys.ToList();
+            list.Sort();
+            foreach (var key in list)
+            {
+                Console.WriteLine("{0}: {1}", key, PrimeMinister[key]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
